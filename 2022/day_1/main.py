b120eb5c6f1dfb04acc6d5c8c85a55ec3e63bce4
hng12 <58843937+hng12@users.noreply.github.com>
2022-12-01 10:13:33 +0000

    Create main.py

---
+++ 2022/day_1/main.py
@@ -0,0 +1,19 @@
+f = open("day1.txt", "r")
+arr = []
+arr.append(f.readline()[0:-1])
+for x in f:
+    if x == '\n':
+        arr.append(f.readline()[0:-1])
+    else:
+        arr[-1] = int(arr[-1]) + int(x[0:-1])
+maxs = []
+for i in range(3):
+    max = [0,0]
+    for i in range(len(arr)):
+        if int(arr[i]) > max[0]:
+            max[0] = int(arr[i])
+            max[1] = i
+    maxs.append(max[0])
+    arr.pop(max[1])
+
+print(sum(maxs))
